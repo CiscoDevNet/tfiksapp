d7b2b05d2c516b0a260693c2e43115baff8b0ac2
prathjan <prathjan@cisco.com>
2021-04-28 15:06:55 -0700

    instal helm chart for helloiks app

---
+++ main.tf
@@ -0,0 +1,39 @@
+#Helm install of sample app on IKS
+data "terraform_remote_state" "iksws" {
+  backend = "remote"
+  config = {
+    organization = "CiscoDevNet"
+    workspaces = {
+      name = var.ikswsname
+    }
+  }
+}
+
+variable "ikswsname" {
+  type = string
+}
+
+resource helm_release iwok8scollector {
+  name       = "iksapp"
+  namespace = "default"
+  chart = "https://prathjan.github.io/helm-chart/helloiks-0.1.0.tgz"
+
+#  set {
+#    name  = "iwoServerVersion"
+#    value = "8.0"
+#  }
+}
+
+provider "helm" {
+  kubernetes {
+    host = local.kube_config.clusters[0].cluster.server
+    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
+    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
+    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
+  }
+}
+
+locals {
+  kube_config = yamldecode(data.terraform_remote_state.iksws.outputs.kube_config)
+}
+
