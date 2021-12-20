e4fb143d63528ff671f689cbe55edf91a7c2e099
prathjan <prathjan@cisco.com>
2021-12-20 08:08:37 -0800

    0.1.1

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ variable "ikswsname" {
 resource helm_release helloiksfrtfcb {
   name       = "helloiksapp"
   namespace = "default"
-  chart = "https://prathjan.github.io/helm-chart/helloiks-0.1.0.tgz"
+  chart = "https://prathjan.github.io/helm-chart/helloiks-0.1.1.tgz"
 
   set {
     name  = "MESSAGE"
